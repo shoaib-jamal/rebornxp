1637b1d6cfdbe15c14deb49976b27982fd0ef44c
Shoaib Jamal <m.shoaib.jamal@outlook.com>
2021-12-17 22:42:03 +0500

    Goodbye 2021

--- config.js
+++ config.js
@@ -20,11 +20,11 @@ function loadConfig(callback) {
     try {
       config = JSON.parse(text);
       console.log(config);
-      xp.wallpaper.set('https://cdn.glitch.com/167aa2fb-3fe2-473a-a11e-1fa5ed692128%2Fbliss.jpg?v=1619083672174');
+      xp.wallpaper.set('https://www.hdwallpapers.in/download/artistic_landscape_minimalist_windows_xp_4k_hd_windows_xp-3840x2160.jpg');
       xp.profile = config.profile;
       xp.theme.set('crystal');
     } catch(e) {
-      xp.wallpaper.set('https://cdn.glitch.com/167aa2fb-3fe2-473a-a11e-1fa5ed692128%2Fbliss.jpg?v=1619083672174');
+      xp.wallpaper.set('https://www.hdwallpapers.in/download/artistic_landscape_minimalist_windows_xp_4k_hd_windows_xp-3840x2160.jpg');
       loadingconfig = false;
       saveConfig();
     }
