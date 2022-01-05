cc08aef5aa55714ab94027eb51d08a87126c02a7
Shoaib Jamal <m.shoaib.jamal@outlook.com>
2022-01-05 13:22:42 +0500

    Better; Eliminated Bugs

--- minesweeper.js
+++ minesweeper.js
@@ -1,9 +1,7 @@
 $(window).on('xpboot', () => {
-  xp.applications.add('minesweeper', (args) => {
-    var guid = generate_guid();
-    var el = $.parseHTML(`<window width="800" height="500" title="Minesweeper">
-    
-    <style>
+  xp.applications.add('minesweeper', () => {
+    var el = $.parseHTML(`<window width="300" height="500" title="Minesweeper">
+      <style>
 iframe[seamless]{
   background-color: transparent;
   border: 0px none transparent;
@@ -22,14 +20,11 @@ iframe[seamless]{
   
   <div class="frame-container">
     
-<iframe height="100%" seamless="seamless" width="100%" src="https://mines.vercel.app/"></iframe>
-
+<iframe height="100%" seamless="seamless" width="100%" src="https://mines.vercel.app"></iframe>
   </div> 
- </window>`);
-    
-document.body.append(el[0]);
-$(el).updateWindow();
-    }
+    </window>`);
+    document.body.appendChild(el[0]);
+    $(el).updateWindow();
   });
-  xp.startmenu.add('minesweeper', 'Minesweeper', '//cdn.glitch.com/01d2e04f-e49d-4304-aa9e-55b9849b4cce%2Fnotepad.png?1521328884236');
+    xp.startmenu.add('minesweeper', 'Minesweeper', '//cdn.glitch.com/01d2e04f-e49d-4304-aa9e-55b9849b4cce%2Fnotepad.png?1521328884236');
 });
