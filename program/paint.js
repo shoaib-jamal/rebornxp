b062eaea1396bd95b1854268d1b8b4ad9e5c55f3
Shoaib Khan <m.shoaib.jamal@outlook.com>
2023-05-20 14:48:21 +0500

    Added Native Paint App
    Thanks to JS Paint

---
+++ program/paint.js
@@ -0,0 +1,30 @@
+$(window).on('xpboot', () => {
+  xp.applications.add('paint', () => {
+    var el = $.parseHTML(`<window width="900" height="600" title="JS Paint">
+      <style>
+iframe[seamless]{
+  background-color: transparent;
+  border: 0px none transparent;
+  padding: 0px;
+  overflow: hidden;
+}
+.frame-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  padding: 0px;
+  margin: 0px;
+}
+  </style>
+  
+  <div class="frame-container">
+    
+<iframe height="100%" seamless="seamless" width="100%" src="https://jspaint.app/"></iframe>
+  </div> 
+    </window>`);
+    document.body.appendChild(el[0]);
+    $(el).updateWindow();
+  });
+    xp.startmenu.add('paint', 'Paint', 'https://raw.githubusercontent.com/1j01/jspaint/master/images/icons/512x512.png');
+});
