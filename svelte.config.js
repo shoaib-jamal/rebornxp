c4916fe036e6b4db2131de572ea9463d3694aeb3
Shoaib Khan <m.shoaib.jamal@outlook.com>
2024-03-25 00:09:13 +0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,20 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-netlify';
 
-export default {
-	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
-	}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter(), // Use the adapter-netlify adapter
+    
+    // Optionally, specify other adapter options, such as `out` for the build output directory
+     adapter: adapter({ out: 'build' })
+  },
+
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 };
+
+export default config;
